bc8946b8c249d3e1335966c5d6ec8cbfa6e6a695
Tanmoy Hazra <tanmoyhazra6969@gmail.com>
2024-08-09 19:26:31 +0530

    New file

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Video() {
   return (
-    <div>
-       <h1 className="max-w-screen-2xl container mx-auto px-0 md:px-6 my-20">404</h1>
-      <h1>Page Not Found....</h1>
+    <div className="max-w-screen-2xl container mx-auto px-0 md:px-6 my-20">
+       <h1 className="text-red-500 text-3xl font-bold mb-5">404</h1>
+      <h1 className="text-3xl font-bold mb-5">Page Not Found....</h1>
     </div>
   )
 }
